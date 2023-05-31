6c3f597c069263b5abf7fa8f26934de89a990356
Franco Marchese <franco.marchese@sulz.cl>
2023-05-31 18:36:43 -0400

    modificada la estructura de los templates

--- index/urls.py
+++ index/urls.py
@@ -4,10 +4,4 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("team/", views.team, name="team"),
-    path("login/", views.login, name="login"),
-    path("", include("hero.urls")),
-    path("", include("stats.urls")),
-    path("", include("news.urls")),
-    path("", include("team.urls")),
-    path("", include("member.urls")),
-]
+    ]
--- nids/settings.py
+++ nids/settings.py
@@ -10,10 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR2 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -41,11 +43,6 @@ INSTALLED_APPS = [
     "theme",
     "django_browser_reload",
     "index",
-    "hero",
-    "stats",
-    "news",
-    "team",
-    "member",
 ]
 
 TAILWIND_APP_NAME = "theme"
@@ -68,7 +65,7 @@ ROOT_URLCONF = "nids.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR2, '/templates/general/home'),],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
