63b095cb62127bf9659ebcb40ac06106b48c0630
Franco Marchese <franco.marchese@sulz.cl>
2023-06-03 21:16:03 -0400

    Creados los modelos y arreglados detalles de los estaticos

--- index/views.py
+++ index/views.py
@@ -11,15 +11,8 @@ import os
 # ---------------------------------------------------
 def home(request):
     nav = ''' 
-            <!-- Menu mobil -->
-            <div class="flex flex-1 justify-end md:hidden">
-            <a href="#stats" class="px-4 text-sm font-semibold leading-6 text-gray-900 hover:text-orange-600">Estadísticas<span aria-hidden="true"></span></a>
-            <a href="#news" class="px-4 text-sm font-semibold leading-6 text-gray-900 hover:text-orange-600">Noticias<span aria-hidden="true"></span></a>
-            <a href="/team" target=blank_ class="px-4 text-sm font-semibold leading-6 text-gray-900 hover:text-orange-600">Equipo<span aria-hidden="true"></span></a>
-            <a href="/login" class="px-4 text-sm font-semibold leading-6 text-gray-900 hover:text-orange-600">Log In<span aria-hidden="true"></span></a>
-            </div>
-            <!-- Menu de escritorio -->
-            <div class="hidden md:flex md:flex-1 md:justify-end">
+            <!-- Menu -->
+            <div class="flex flex-1 justify-end">
             <a href="#stats" class="px-4 text-sm font-semibold leading-6 text-gray-900 hover:text-orange-600">Estadísticas<span aria-hidden="true"></span></a>
             <a href="#news" class="px-4 text-sm font-semibold leading-6 text-gray-900 hover:text-orange-600">Noticias<span aria-hidden="true"></span></a>
             <a href="/team" target=blank_ class="px-4 text-sm font-semibold leading-6 text-gray-900 hover:text-orange-600">Equipo<span aria-hidden="true"></span></a>
@@ -33,16 +26,9 @@ def home(request):
 def team(request):
     nav = '''
             <!-- Menu mobil -->
-            <div class="flex flex-1 justify-end md:hidden">
+            <div class="flex flex-1 justify-end">
             <a href="/" class="px-4 text-sm font-semibold leading-6 text-gray-900 hover:text-orange-600">Inicio<span aria-hidden="true"></span></a>
             <a href="/login" class="px-4 text-sm font-semibold leading-6 text-gray-900 hover:text-orange-600">Log In<span aria-hidden="true"></span></a>
-            </div>
-            <!-- Menu de escritorio -->
-            <div class="hidden md:flex md:flex-1 md:justify-end">
-            <a href="/" class="px-4 text-sm font-semibold leading-6 text-gray-900 hover:text-orange-600">Inicio<span aria-hidden="true"></span></a>
-            <div class="flex flex-row">
-                <a href="/login" class="px-4 text-sm font-semibold leading-6 text-gray-900 hover:text-orange-600"><span aria-hidden="true">Log In</span></a>
-            </div>
             </div>
         '''
     return render(request, 'equ-team.html', {
@@ -53,16 +39,10 @@ def team(request):
 def profile(request):
     nav = '''
         <!-- Menu mobil -->
-        <div class="flex flex-1 justify-end md:hidden">
+        <div class="flex flex-1 justify-end">
         <a href="/" class="px-4 text-sm font-semibold leading-6 text-gray-900 hover:text-orange-600">Inicio<span aria-hidden="true"></span></a>
         <a href="/team" class="px-4 text-sm font-semibold leading-6 text-gray-900 hover:text-orange-600">Equipo<span aria-hidden="true"></span></a>
         <a href="/login" class="px-4 text-sm font-semibold leading-6 text-gray-900 hover:text-orange-600">Log In<span aria-hidden="true"></span></a>
-        </div>
-        <!-- Menu de escritorio -->
-        <div class="hidden md:flex md:flex-1 md:justify-end">
-        <a href="/" class="px-4 text-sm font-semibold leading-6 text-gray-900 hover:text-orange-600">Inicio<span aria-hidden="true"></span></a>
-        <a href="/team" class="px-4 text-sm font-semibold leading-6 text-gray-900 hover:text-orange-600">Equipo<span aria-hidden="true"></span></a>
-        <a href="/login" class="px-4 text-sm font-semibold leading-6 text-gray-900 hover:text-orange-600"><span aria-hidden="true">Log In</span></a>
         </div>
     '''
     return render(request, 'equ-profile.html', {
@@ -72,15 +52,9 @@ def profile(request):
 def login(request):
     nav = '''
         <!-- Menu mobil -->
-        <div class="flex flex-1 justify-end md:hidden">
+        <div class="flex flex-1 justify-end">
         <a href="/" class="px-4 text-sm font-semibold leading-6 text-gray-900 hover:text-orange-600">Inicio<span aria-hidden="true"></span></a>
         <a href="/team" class="px-4 text-sm font-semibold leading-6 text-gray-900 hover:text-orange-600">Equipo<span aria-hidden="true"></span></a>
-        </div>
-        <!-- Menu de escritorio -->
-        <div class="hidden md:flex md:flex-1 md:justify-end">
-        <a href="/" class="px-4 text-sm font-semibold leading-6 text-gray-900 hover:text-orange-600">Inicio<span aria-hidden="true"></span></a>
-        <a href="/team" class="px-4 text-sm font-semibold leading-6 text-gray-900 hover:text-orange-600">Equipo<span aria-hidden="true"></span></a>
-
         </div>
     '''
     return render(request, 'usu-login.html', {
